0bea9c04eac0ca20df4ce29c5f3ed193c17718bc
sinkline <popovartem505@gmail.com>
2019-08-01 18:22:39 +0300

    dz finished

---
+++ лекция 3/dz/dz.py
@@ -0,0 +1,19 @@
+while True:
+
+	b = input('Введите текст: ')
+	z = (b.lower())
+	w = z.replace(" ", "")
+	l = "".join(w for w in w if w not in ('!','.',':',',','?',';','-','_','|'))
+	w = l
+	s = w
+	(list(s))
+	a = ("".join(s[::-1]))
+
+	p = ('stop')
+	if b == p:
+		exit()
+
+	if s == a:
+	  print("Ваш текст является полиндромом!")
+	else:
+	  print("Ваш текст не является полиндромом!")
